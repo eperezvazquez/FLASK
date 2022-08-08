28c97419d477251259056396e906a37899bd962b
eperezvazquez <perezelisa21@gmail.com>
2022-08-08 21:58:52 +0000

    Acabo de actulizar el requirement

--- src/test.py
+++ src/test.py
@@ -2,21 +2,20 @@ import pickle
 import pandas as pd
 import numpy as np
 
-country="other"
-variety="other"
-aroma= 7.42
-aftertaste= 7.33
-acidity= 7.42
-body= 7.25
+country = 'Other'
+variety = 'Other'
+aroma = 7.42
+aftertaste = 7.33
+acidity = 7.42
+body = 7.25
 balance = 7.33
-moisture= 0.0
+moisture = 0.0
 
-cols= ['country_of_origin','variety','aroma','aftertaste','acidity','body','balance','moisture']
-data= [country,variety,aroma,aftertaste,acidity,body,balance,moisture]
-posted= pd.DataFrame(np.array(data).reshape(1,8),columns=cols)
+cols = ['country_of_origin', 'variety', 'aroma', 'aftertaste', 'acidity', 'body', 'balance', 'moisture']
+data = [country, variety, aroma, aftertaste, acidity, body, balance, moisture]
+posted = pd.DataFrame(np.array(data).reshape(1,8), columns= cols)
 
-loaded_model=pickle.load(open('../models/coffee_model.pkl','rb'))
-result=loaded_model.predict(posted)
-text_result=result.tolist()[0]
+loaded_model = pickle.load(open('../models/coffee_model.pkl', 'rb'))
+result = loaded_model.predict(posted)
+text_result = result.tolist(result)[0]
 print(text_result)
-
