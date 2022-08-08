2876ee18408e311759755b7585de5f999bce8b7e
eperezvazquez <perezelisa21@gmail.com>
2022-08-08 21:57:21 +0000

    Acabo de actulizar el requirement

--- src/test.py
+++ src/test.py
@@ -11,9 +11,9 @@ body= 7.25
 balance = 7.33
 moisture= 0.0
 
-cols= ['country_of_origin','variety','aroma','aftertaste','acidity','body','balance','moisure']
+cols= ['country_of_origin','variety','aroma','aftertaste','acidity','body','balance','moisture']
 data= [country,variety,aroma,aftertaste,acidity,body,balance,moisture]
-posted=pd.dataframe(np.array(data).reshape(1,8),columns=cols)
+posted= pd.DataFrame(np.array(data).reshape(1,8),columns=cols)
 
 loaded_model=pickle.load(open('../models/coffee_model.pkl','rb'))
 result=loaded_model.predict(posted)
