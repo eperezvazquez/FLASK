cdc73fd23587ab8902288d17685c56fa76aff59c
eperezvazquez <perezelisa21@gmail.com>
2022-08-08 22:00:35 +0000

    Acabo de actulizar el requirement

--- src/test.py
+++ src/test.py
@@ -2,20 +2,41 @@ import pickle
 import pandas as pd
 import numpy as np
 
-country = 'Other'
-variety = 'Other'
-aroma = 7.42
-aftertaste = 7.33
-acidity = 7.42
-body = 7.25
-balance = 7.33
-moisture = 0.0
+# Contenido ficticio para poner en el model
+# Modelo recibe 8 variables
 
+country='Colombia'
+variety='Caturra'
+aroma = 7.83
+aftertaste = 7.77
+acidity = 7.33
+body=7.67
+balance=7.77
+moisture=0.11
+
+
+# Usamos un pipeline entonces tenemos que ponerlo en un df
+
+# cabecera de objeto pandas
 cols = ['country_of_origin', 'variety', 'aroma', 'aftertaste', 'acidity', 'body', 'balance', 'moisture']
-data = [country, variety, aroma, aftertaste, acidity, body, balance, moisture]
-posted = pd.DataFrame(np.array(data).reshape(1,8), columns= cols)
 
-loaded_model = pickle.load(open('../models/coffee_model.pkl', 'rb'))
-result = loaded_model.predict(posted)
+# contenido de objeto pandas
+data = [country, variety, aroma, aftertaste, acidity, body, balance, moisture]
+
+# df
+posted = pd.DataFrame(np.array(data).reshape(1,8), columns=cols)
+#print(posted)
+
+
+# Cargamos modelo entrenado
+
+loaded_model = pickle.load(open('../models/coffee_model.pkl', 'rb')) # rb: read binary
+
+
+# Pasar los datos al modelo
+
+result = loaded_model.predict(posted) # devuelve archivo np, necesito llevarlo a texto
+
 text_result = result.tolist()[0]
+
 print(text_result)
