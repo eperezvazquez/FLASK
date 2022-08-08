ae3729e887ef73d8dca85044b9f155578bb2029e
eperezvazquez <perezelisa21@gmail.com>
2022-08-08 22:38:30 +0000

    Acabo de actulizar el requirement

--- src/app.py
+++ src/app.py
@@ -1,32 +1,26 @@
-##ejemplo de API
-from flask import Flask, render_template, jsonify, request 
 import pickle
 import pandas as pd
 import numpy as np
 
-app = Flask(__name__) # hacer ref al nombre del archivo
+country='Other' # Si agrego una categoria que no existe en en la lista, salta un error
+variety='Other'
+aroma=7.42
+aftertaste= 7.32
+acidity=7.42
+body=7.25
+balance= 7.33
+moisture=0.0
 
-@app.route('/')
-def hello_flask():
-    return 'Hello Flask'
+#Como es un pipline no se pude ingresar directamente, si fuera una lista si. En este caso se hace un pandas.
 
-@app.route('/inicio')
-def show_home():
-    return render_template('Index.html')
+cols= ['country_of_origin','variety','aroma','aftertaste','acidity','body','balance','moisture'] # cabecera de pandas
+data=[country,variety,aroma,aftertaste,acidity,body,balance,moisture] # datos
 
-@app.route('/<string:country>/<string:variety>/<float:aroma>/<float:aftertaste>/<float:acidity>/<float:body>/<float:balance>/<float:moisture>/')
-def result(country, variety, aroma, aftertaste, acidity, body, balance, moisture):
-    cols = ['country_of_origin', 'variety', 'aroma', 'aftertaste', 'acidity', 'body', 'balance', 'moisture']   
-    data = [country, variety, aroma, aftertaste, acidity, body, balance, moisture]
-    posted = pd.DataFrame(np.array(data).reshape(1,8), columns=cols)
-    loaded_model = pickle.load(open('../models/coffee_model.pkl', 'rb')) # rb: read binary
-    result = loaded_model.predict(posted) # devuelve archivo np, necesito llevarlo a texto
-    text_result = result.tolist()[0]
-    if text_result=='Yes':
-        return jsonify(message='Es un cafe de primera'),200
-    else:
-        return jsonify(message='No es un cafe de primera'),200
+posted=pd.DataFrame(np.array(data).reshape(1,8),columns=cols) # se creo el archivo
+# falta abrir el archivo pickle
+loaded_model=pickle.load(open('../models/coffee_model.pkl','rb'))
+result=loaded_model.predict(posted)
+text_result=result.tolist()[0]
+print(text_result)
 
-if __name__ == '__main__':
-    app.run(debug= True, host='127.0.0.1', port=5000) 
-
+# modulo para hacer pruebas pytest
--- src/test.py
+++ src/test.py
@@ -1,42 +1,37 @@
+from flask import Flask,render_template,jsonify,request
 import pickle
 import pandas as pd
 import numpy as np
 
-# Contenido ficticio para poner en el model
-# Modelo recibe 8 variables
+app=Flask(__name__)
+@app.route('/')
+def hello_flask():
+    return 'Hello Flask'
 
-country='Colombia'
-variety='Caturra'
-aroma = 7.83
-aftertaste = 7.77
-acidity = 7.33
-body=7.67
-balance=7.77
-moisture=0.11
+@app.route('/inicio')
+def show_home():
+    return render_template('index.html')
 
+#@app.route('/url_variables/<string:name>/<int:age>')
+#def url_variables(name,age):
+ #   if age<18:
+  #      return jsonify(message='Lo siento '+ name +' no autorizado'), 401 # modifica el texto a json que es el lenguaje de la web
+   # else:
+    #    return jsonify(message='Bienvenido ' + name), 200
 
-# Usamos un pipeline entonces tenemos que ponerlo en un df
+@app.route('/<string:country>/<string:variety>/<float:aroma>/<float:aftertaste>/<float:acidity>/<float:body>/<float:balance>/<float:moisture>') # se dice al Flask el tipo de datos que va a recibir segun el test.py
+def result(country,variety,aroma,aftertaste,acidity,body,balance,moisture):
+    cols= ['country_of_origin','variety','aroma','aftertaste','acidity','body','balance','moisture'] # cabecera de pandas
+    data=[country,variety,aroma,aftertaste,acidity,body,balance,moisture] # datos
+    posted=pd.DataFrame(np.array(data).reshape(1,8),columns=cols) # se creo el archivo
+    # falta abrir el archivo pickle
+    loaded_model=pickle.load(open('../models/coffee_model.pkl','rb'))
+    result=loaded_model.predict(posted)
+    text_result=result.tolist()[0]
+    if text_result=='Yes':
+        return jsonify(message='Es un cafe de especialidad'), 200
+    else:
+        return jsonify(message='No es un cafe de especialidad'), 200
 
-# cabecera de objeto pandas
-cols = ['country_of_origin', 'variety', 'aroma', 'aftertaste', 'acidity', 'body', 'balance', 'moisture']
-
-# contenido de objeto pandas
-data = [country, variety, aroma, aftertaste, acidity, body, balance, moisture]
-
-# df
-posted = pd.DataFrame(np.array(data).reshape(1,8), columns=cols)
-#print(posted)
-
-
-# Cargamos modelo entrenado
-
-loaded_model = pickle.load(open('../models/coffee_model.pkl', 'rb')) # rb: read binary
-
-
-# Pasar los datos al modelo
-
-result = loaded_model.predict(posted) # devuelve archivo np, necesito llevarlo a texto
-
-text_result = result.tolist()[0]
-
-print(text_result)
+if __name__=='__main__':
+    app.run(debug=True,host='127.0.0.1',port=5000) #plataforma 127.0.0.1
